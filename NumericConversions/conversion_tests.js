c343a932d895acbb888a53ed359397a3f8eb0db6
harrisonzana <harrisonzana@gmail.com>
2024-04-24 14:31:50 -0400

    completed conversion tests exercise

---
+++ NumericConversions/conversion_tests.js
@@ -0,0 +1,54 @@
+"use strict"
+
+// Description: This script tests various numeric
+//              conversion techniques
+// Author: Jordan Q. Newprogrammer
+
+var a = " 101.1 ";
+var b = "55";
+var c = "402 Stevens";
+var d = "Number 5";
+
+// for var a
+
+
+// console.log(a);
+//var aInt = parseInt(a);
+//console.log(aInt);
+//var aFloat = parseFloat(a);
+//console.log(aFloat);
+
+
+// var a
+
+console.log(a)
+parseInt(a)
+parseFloat(a)
+Number(a)
+
+// var b
+
+console.log(b)
+parseInt(b)
+parseFloat(b)
+Number(b)
+
+
+// var c
+
+console.log(c)
+parseInt(c)
+parseFloat(c)
+Number(c)
+
+// var d
+
+console.log(d)
+parseInt(d)
+parseFloat(d)
+Number(d)
+
+// var name = "zana";
+// console.log(name);
+// var nameInt = parseInt(name);
+// console.log(nameInt);
